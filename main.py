b1d44cf52dfec6dbe28a5a1af84a26f2b26eb9f0
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-07-03 10:22:00 +0300

    add the loading environment functionality to routes

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+
 from routes import base
 
 app = FastAPI()
--- routes/base.py
+++ routes/base.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, APIRouter
+import os
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix='/api/v1',
+    tags=['api_v1']
+)
 
 @base_router.get("/")
-def welcome():
+async def welcome():
+    app_name = os.getenv('APP_NAME')
+    app_version = os.getenv('APP_VERSION')
+
     return {
-        "message":"Hello world"
+        "app name": app_name,
+        "app version": app_version,
     }
