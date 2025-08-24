e9b6a436a10a113ffa2c6a8e5e5159ffbb3f102f
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-08-24 12:44:02 +0300

    create VectorDBFactoryProvider

---
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -0,0 +1,20 @@
+from .providers import QdrantProvider
+from .VectorDBEnums import VectorDBEnums
+from controllers.BaseController import BaseController
+
+class VectorDBProviderFactory():
+    
+    def __init__(self, config):
+        self.config = config
+        self.base_controller = BaseController()
+    
+    def create(self, provider: str):
+        if provider == VectorDBEnums.QDRANT.value:
+            db_path = self.base_controller.get_database_path(self.config.VECTOR_DB_PATH)
+            
+            return QdrantProvider(
+                db_path=db_path,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+            )
+        
+        return None
