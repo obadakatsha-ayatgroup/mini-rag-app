0014d7095abf68ac934a487252db7f3caf22441a
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-01 15:19:18 +0300

    add get project chunks

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -62,3 +62,15 @@ class ChunkModel(BaseDataModel):
         })
 
         return result.deleted_count
+    
+    async def get_projects_chunks(self, project_id: ObjectId, page_no: int=1, page_size:int=50):
+        records = await self.collection.find({
+            "chunk_project_id": project_id
+        }).skip(
+            (page_no-1) * page_size
+        ).limit(page_size).to_list(length=None)
+
+        return [
+            DataChunk(**rec)
+            for rec in records
+        ]
