6e03d03496d7e2a63efc7c550626ec7c29c2fee0
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-01 15:21:25 +0300

    change onstartup and onshutdown

--- src/main.py
+++ src/main.py
@@ -4,17 +4,20 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings 
 from contextlib import asynccontextmanager
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
+from routes import nlp
 
 
 app = FastAPI()
 
 
-async def startup_dp_client():
+async def startup_span():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
     llm_factory_provider = LLMProviderFactory(config=settings)
+    vector_db_provider_factory = VectorDBProviderFactory(config=settings)
 
     #generation client
     app.generation_client = llm_factory_provider.create(provider=settings.GENERATION_BACKEND)
@@ -24,15 +27,24 @@ async def startup_dp_client():
     app.embedding_client = llm_factory_provider.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE)
 
+    #vector db client
+    app.vectordb_client = vector_db_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
 
-async def shutdown_db_client():
+async def shutdown_span():
     app.mongo_conn.close()
+    app.vectordb_client.disconnect()
 
 
-app.router.lifespan.on_startup.append(startup_dp_client)
-app.router.lifespan.on_shutdown.append(shutdown_db_client)
+# app.router.lifespan.on_startup.append(startup_span)
+# app.router.lifespan.on_shutdown.append(shutdown_span)
+
+app.on_event("startup")(startup_span)
+app.on_event("shutdown")(shutdown_span)
 
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
+
 
