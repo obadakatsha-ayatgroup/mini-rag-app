64d05470be10da288e61478ca2991a5959cfceaf
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-03 14:17:41 +0300

    update ResponseSignalEnum with signals related to RAG answers

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -17,3 +17,5 @@ class ResponseSignal(Enum):
     VECTORDB_COLLECTION_RETRIEVED = " vectordb collection retrieved"
     VECTORDB_SEARCH_ERROR = "vectordb seach error"
     VECTORDB_SEARCH_SUCCESS = "vectordb search success"
+    RAG_ANSWER_ERROR = "rag answer error"
+    RAG_ANSWER_SUCCESS = "rag answer success"
