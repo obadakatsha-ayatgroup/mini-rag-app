677e8fdae79413be7a39404f67e5398865ffa07b
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-03 14:21:17 +0300

    add answer rag endpoint with the template parser functionality

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -125,7 +125,8 @@ async def search_index(request: Request, project_id: str, search_request: Search
         )
     nlp_controller = NLPController(vectordb_client=request.app.vectordb_client,
                                 generation_client=request.app.generation_client,
-                                embedding_client=request.app.embedding_client)
+                                embedding_client=request.app.embedding_client,
+                                template_parser=request.app.template_parser)
     
     results = nlp_controller.search_vectordb_collection(project=project, text=search_request.text, limit=search_request.limit)
 
@@ -140,6 +141,48 @@ async def search_index(request: Request, project_id: str, search_request: Search
     return JSONResponse(
         content={
             "signal": ResponseSignal.VECTORDB_SEARCH_SUCCESS.value,
-            "results": results
+            "results": [ res.dict() for res in results]
+        }
+    )
+
+@nlp_router.post('index/answer/{project_id}')
+async def answer_rag(request: Request, project_id: str, search_request: SearchRequest):
+    
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
+
+    project = await project_model.get_project_or_create_one(project_id=project_id)
+
+
+    if not project:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROJECT_NOT_FOUND_ERROR.value
+            }
+        )
+    nlp_controller = NLPController(vectordb_client=request.app.vectordb_client,
+                                generation_client=request.app.generation_client,
+                                embedding_client=request.app.embedding_client,
+                                template_parser=request.app.template_parser)
+    
+    answer, full_prompt, chat_history = nlp_controller.answer_reg_question(
+        project=project,
+        query=search_request.text,
+        limit=search_request.limit )
+    
+    if not answer:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.RAG_ANSWER_ERROR.value
+            }
+        )
+    
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.RAG_ANSWER_SUCCESS.value,
+            "answer": answer,
+            "full prompt": full_prompt,
+            "chat history": chat_history
         }
     )
