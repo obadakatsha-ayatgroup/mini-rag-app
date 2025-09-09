e324684309fc036bf4d3d99fb3a54efb5b64c229
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-09 12:19:57 +0300

    remove mongodb credentials and add postgres's

--- src/main.py
+++ src/main.py
@@ -1,40 +1,46 @@
 from fastapi import FastAPI
 from routes import base, data
-from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings 
-from contextlib import asynccontextmanager
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 from routes import nlp
 from stores.llm.templates.template_parser import TemplateParser
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
 
 async def startup_span():
     settings = get_settings()
-    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+    postgres_conn = f"postgresql+asyncpg://:{settings.POSTGRES_USERNAME}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_MAIN_DATABASE}"
+
+    app.db_engine = create_async_engine(postgres_conn)
+
+    app.db_client = sessionmaker(
+        app.db_engine, class_=AsyncSession, expire_on_commit=False 
+    )
 
     llm_factory_provider = LLMProviderFactory(config=settings)
     vector_db_provider_factory = VectorDBProviderFactory(config=settings)
 
-    #generation client
+    # generation client
     app.generation_client = llm_factory_provider.create(provider=settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
 
-    #embedding client
+    # embedding client
     app.embedding_client = llm_factory_provider.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE)
 
-    #vector db client
+    # vector db client
     app.vectordb_client = vector_db_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
     app.vectordb_client.connect()
 
     app.template_parser = TemplateParser(language=settings.PRIMARY_LANGUAGE, default_language=settings.DEFAULT_LANGUAGE)
 
 async def shutdown_span():
-    app.mongo_conn.close()
+    app.db_engine.dispose()
     app.vectordb_client.disconnect()
 
 
