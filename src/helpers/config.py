9f4702a533098ae89b2c9234eb21a34a46ce59f1
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-09 12:19:20 +0300

    add  postgres variables to get from .env

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -9,8 +9,11 @@ class Settings(BaseSettings):
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE:int
 
-    MONGODB_DATABASE: str
-    MONGODB_URL: str
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_MAIN_DATABASE: str
 
     GENERATION_BACKEND: str
     EMBEDDING_BACKEND: str
