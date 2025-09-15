2b9d7e84cf2a0c03846a78a5dbb0f52e4531d71c
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-15 15:21:18 +0300

    convert NLPController functions to be async

--- src/controllers/NLPController.py
+++ src/controllers/NLPController.py
@@ -15,72 +15,76 @@ class NLPController(BaseController):
         self.template_parser = template_parser
 
     def create_collection_name(self, project_id: str):
-        return f"collection_{project_id}".strip()
+        return f"collection_{self.vectordb_client.default_vector_size}_{project_id}".strip()
     
-    def reset_vectordb_collection(self, project: Project):
+    async def reset_vectordb_collection(self, project: Project):
         collection_name = self.create_collection_name(project_id=project.project_id)
-        return self.vectordb_client.delete_collection(collection_name=collection_name)
+        return await self.vectordb_client.delete_collection(collection_name=collection_name)
     
-    def get_vectordb_collection_info(self, project: Project):
+    async def get_vectordb_collection_info(self, project: Project):
         collection_name = self.create_collection_name(project_id=project.project_id)
-        collection_info = self.vectordb_client.get_collection_info(collection_name=collection_name)
+        collection_info = await self.vectordb_client.get_collection_info(collection_name=collection_name)
 
         return json.loads(
             json.dumps(collection_info, default=lambda x: x.__dict__)
         )
     
-    def index_into_vectordb(self, project: Project, chunks: List[DataChunk], chunks_ids: list, do_reset:int=0):
+    async def index_into_vectordb(self, project: Project, chunks: List[DataChunk], chunks_ids: list, do_reset:int=0):
         # get collection name
         collection_name = self.create_collection_name(project_id=project.project_id)
 
         # manage items
         text = [record.chunk_text for record in chunks]
-        meta_data = [record.chunk_metadata for record in chunks]
+        metadata = [record.chunk_metadata for record in chunks]
 
-        vectors = [
-            self.embedding_client.embed_txt(txt=txt, document_type=DocumentType.DOCUMENT.value)
-            for txt in text
-        ]
+        vectors = self.embedding_client.embed_txt(txt=text, document_type=DocumentType.DOCUMENT.value)
 
         # create collection if exist
-        _ = self.vectordb_client.create_collection(collection_name=collection_name,
+        _ = await self.vectordb_client.create_collection(collection_name=collection_name,
                           embedding_size=self.embedding_client.embedding_size,
                           do_reset=do_reset)
         # insert into vectordb
-        _ = self.vectordb_client.insert_many(collection_name=collection_name,
+        _ = await self.vectordb_client.insert_many(collection_name=collection_name,
                                             txts=text,
                                             vectors=vectors,
-                                            meta_data=meta_data,
+                                            metadata=metadata,
                                             record_ids=chunks_ids
                                             )
         
         return True
     
-    def search_vectordb_collection(self, project: Project, text: str, limit: int=10):
+    async def search_vectordb_collection(self, project: Project, text: str, limit: int=10):
 
+        query_vector = None
         # get collection name
         collection_name = self.create_collection_name(project_id=project.project_id)
 
         # get text embedding vector
-        vector = self.embedding_client.embed_txt(txt=text, document_type=DocumentType.QUERY.value)
+        vectors = self.embedding_client.embed_txt(txt=text, document_type=DocumentType.QUERY.value)
 
-        if not vector or len(vector)==0:
+        if not vectors or len(vectors)==0:
             return False
         
+        if isinstance(vectors, list) and len(vectors) > 0:
+            query_vector = vectors[0]
+        
+        if not query_vector:
+            return False
+
         # do semantic search
-        results = self.vectordb_client.search_by_vector(collection_name=collection_name, vector=vector, limit=limit)
+        results = await self.vectordb_client.search_by_vector(collection_name=collection_name, vector=query_vector, limit=limit)
 
         if not results:
             return False
         
         return results
     
-    def answer_reg_question(self, project: Project, query: str, limit: int=10):
+    async def answer_reg_question(self, project: Project, query: str, limit: int=10):
         
         answer, full_prompt, chat_history = None, None, None
 
         # get collection name
-        retrieved_documents = self.search_vectordb_collection(
+        retrieved_documents = await self.search_vectordb_collection(
             project=project,
             text=query,
             limit=limit
