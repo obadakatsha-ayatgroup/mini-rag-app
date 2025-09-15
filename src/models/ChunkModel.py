ea30bc71737507edb44b607e178dfec868f91f87
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-15 15:20:33 +0300

    add function to get total count of chunks withing db

--- src/models/ChunkModel.py
+++ src/models/ChunkModel.py
@@ -55,4 +55,11 @@ class ChunkModel(BaseDataModel):
             result = await session.execute(stmt)
             records = result.scalars().all()
         return records
-        
+    
+    async def get_total_count_of_chunks(self, project_id: ObjectId):
+        total_count = 0
+        async with self.db_client() as session:
+            count_sql = select(func.count(DataChunk.chunk_id)).where(DataChunk.chunk_project_id==project_id)
+            records_count = await session.execute(count_sql)
+            total_count = records_count.scalar() 
+        return total_count
