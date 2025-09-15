0c2675c2e79307846539decffa97778e6192a54f
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-15 15:25:47 +0300

    add the PgVectorDB provider to the factory and standardize variables between providers

--- src/stores/vectordb/VectorDBProviderFactory.py
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -1,19 +1,31 @@
-from .providers.QdrantProvider import QdrantProvider
+from .providers import QdrantProvider, PgVectorProvider
 from .VectorDBEnums import VectorDBEnums
 from controllers.BaseController import BaseController
-
+from sqlalchemy.orm import sessionmaker
 class VectorDBProviderFactory():
     
-    def __init__(self, config):
+    def __init__(self, config, db_client: sessionmaker=None):
         self.config = config
         self.base_controller = BaseController()
+        self.db_client = db_client
     
     def create(self, provider: str):
         if provider == VectorDBEnums.QDRANT.value:
-            db_path = self.base_controller.get_database_path(self.config.VECTOR_DB_PATH)
+            qdrant_db_client = self.base_controller.get_database_path(self.config.VECTOR_DB_PATH)
             return QdrantProvider(
-                db_path=db_path,
+                db_client=qdrant_db_client,
                 distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD,
+                default_vector_size=self.config.EMBEDDING_MODEL_SIZE
             )
+        
+        if provider == VectorDBEnums.PGVECTOR.value:
+            return PgVectorProvider(
+                db_client=self.db_client,
+                distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
+                index_threshold=self.config.VECTOR_DB_PGVEC_INDEX_THRESHOLD,
+                default_vector_size=self.config.EMBEDDING_MODEL_SIZE
+            )
+
         
         return None
