29c834ad3939d8fa2ac58708a62d5bdf881dd132
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-09-18 09:54:43 +0300

    add setup_metrics that follows metrics

--- src/main.py
+++ src/main.py
@@ -7,9 +7,11 @@ from routes import nlp
 from stores.llm.templates.template_parser import TemplateParser
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from utils.metrics import setup_metrics
 
 app = FastAPI()
 
+setup_metrics(app=app)
 
 async def startup_span():
     settings = get_settings()
